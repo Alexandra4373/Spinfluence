f03570c8e8ed3f0bf64ea2540cf4c963969d2d75
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-10-14 16:53:59 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import logo from "./assets/logo.png";
+import menu from "./assets/menu.png";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="">
+        {/* Nav and hero */}
+        <div className="">
+          <div className="container">
+            {/* Nav */}
+            <div className="flex items-center justify-between py-4 ">
+              <div className="">
+                <img src={logo} />
+              </div>
+              <ul className="flex gap-5 cursor-pointer">
+                <li>Home</li>
+                <li>Service</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+              <div className="flex gap-5">
+                <h6>Login</h6>
+                <img src={menu} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
